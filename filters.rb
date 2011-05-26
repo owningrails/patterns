0b3377a857a23ab611ed12d092a6a6de969e36c1
macournoyer <macournoyer@gmail.com>
2011-05-26 12:49:22 -0400

    Refactoring filters.

--- filters.rb
+++ filters.rb
@@ -1,40 +1,28 @@
 require "active_support/all"
 
 module Filters
-  def self.included(base)
-    base.extend ClassMethods
-    base.class_attribute :before_filters
-    base.before_filters = []
-    base.class_attribute :after_filters
-    base.after_filters = []
-    base.class_attribute :around_filters
-    base.around_filters = []
+  extend ActiveSupport::Concern
+  include ActiveSupport::Callbacks
+  
+  included do
+    define_callbacks :process
   end
   
   module ClassMethods
     def before_filter(method)
-      self.before_filters += [method]
+      set_callback :process, :before, method
     end
     def after_filter(method)
-      self.after_filters += [method]
+      set_callback :process, :after, method
     end
     def around_filter(method)
-      self.around_filters += [method]
+      set_callback :process, :around, method
     end
   end
   
   def filter
-    process_proc = proc do
-      before_filters.each { |method| send(method) }
+    run_callbacks :process do
       yield
-      after_filters.each { |method| send(method) }
     end
-    
-    around_filters.reverse.each do |method|
-      proc = process_proc
-      process_proc = proc { send(method, &proc) }
-    end
-    
-    process_proc.call
   end
 end
