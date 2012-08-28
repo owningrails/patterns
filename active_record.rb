4e2d896db7a218fadeec7a0e38dd821a3eaa2d01
macournoyer <macournoyer@gmail.com>
2012-08-28 10:19:02 -0400

    Implement AR.

---
+++ active_record.rb
@@ -0,0 +1,47 @@
+require "connection_adapter"
+
+module ActiveRecord
+  class Base
+    @@connection = SqliteAdapter.new
+
+    def initialize(attributes={})
+      @attributes = attributes
+    end
+
+    def method_missing(name, *args)
+      if self.class.columns.include?(name)
+        @attributes[name]
+      else
+        super
+      end
+    end
+
+    def self.find(id)
+      find_by_sql("SELECT * FROM #{table_name} WHERE id = #{id.to_i} LIMIT 1").first
+    end
+
+    def self.all
+      find_by_sql("SELECT * FROM #{table_name}")
+    end
+
+    def self.find_by_sql(sql)
+      rows = @@connection.execute(sql)
+      rows.map do |row|
+        attributes = map_values_to_columns(row)
+        new(attributes)
+      end
+    end
+
+    def self.map_values_to_columns(values)
+      Hash[columns.zip(values)]
+    end
+
+    def self.columns
+      @@connection.columns(table_name)
+    end
+    
+    def self.table_name
+      name.downcase + "s" # "users"
+    end
+  end
+end
---
+++ tests/user_test.rb
@@ -0,0 +1,43 @@
+require File.dirname(__FILE__) + '/test_helper'
+require "active_record"
+require "models/user"
+
+class UserTest < Test::Unit::TestCase
+  def test_intialize_with_attributes
+    user = User.new(:id => 1, :name => "Marc")
+    assert_equal 1, user.id
+    assert_equal "Marc", user.name
+  end
+  
+  def test_find
+    user = User.find(1)
+    assert_equal 1, user.id
+  end
+  
+  def test_all
+    user = User.all.first
+    assert_equal 1, user.id
+  end
+  
+  def test_map_values_to_columns
+    values = [1, "Marc"]
+    columns = [:id, :name]
+    
+    # attributes = {}
+    # columns.each_with_index do |column, index|
+    #   attributes[column] = values[index]
+    # end
+    
+    attributes = User.map_values_to_columns(values)
+    
+    assert_equal({:id => 1, :name => "Marc"}, attributes)
+  end
+  
+  def test_columns
+    assert_equal [:id, :name], User.columns
+  end
+  
+  def test_table_name
+    assert_equal "users", User.table_name
+  end
+end
