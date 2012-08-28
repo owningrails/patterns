5b4cbef328943627438c9660c3f45ab5d1aab820
macournoyer <macournoyer@gmail.com>
2012-08-28 12:45:23 -0400

    Implement after_filter and DO THE IMPOSSIBLE!

--- tests/filters_test.rb
+++ tests/filters_test.rb
@@ -2,8 +2,11 @@ require File.dirname(__FILE__) + '/test_helper'
 require "controller"
 
 class TestController < Controller
+  around_filter :around1
+  around_filter :around2
   before_filter :one
   before_filter :two
+  after_filter :three
   
   def initialize(out)
     @out = out
@@ -16,11 +19,28 @@ class TestController < Controller
   def two
     @out << :two
   end
+  
+  def three
+    @out << :three
+  end
+  
+  def around1
+    @out << "{"
+    yield
+    @out << "}"
+  end
+  
+  def around2
+    @out << "["
+    yield
+    @out << "]"
+  end
 end
 
 class FiltersTest < Test::Unit::TestCase
   def test_store_filters
     assert_equal [:one, :two], TestController.before_filters
+    assert_equal [:three], TestController.after_filters
   end
   
   def test_filtering
@@ -28,6 +48,6 @@ class FiltersTest < Test::Unit::TestCase
     TestController.new(out).filter do
       out << :process
     end
-    assert_equal [:one, :two, :process], out
+    assert_equal ["{", "[", :one, :two, :process, :three, "]", "}"], out
   end
 end
