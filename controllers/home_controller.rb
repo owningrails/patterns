2bff0841d5ac2e6e1e4e0129e2391c973a98499c
macournoyer <macournoyer@gmail.com>
2012-08-28 12:17:38 -0400

    Setup after_filter & around_filter.

--- controllers/home_controller.rb
+++ controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < Controller
+  # around_filter :layout
   before_filter :header
+  after_filter :footer
   
   def index
     response.write "Hello from home controller!"
@@ -12,4 +14,14 @@ class HomeController < Controller
   def header
     response.write "<h1>My App</h1>"
   end
+  
+  def footer
+    response.write "<p>&copy; me</p>"
+  end
+  
+  # def layout
+  #   response.write "<html><body>"
+  #   yield
+  #   response.write "</body></html>"
+  # end
 end
