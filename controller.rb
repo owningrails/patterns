c95d6005d02a7ff28d938ce6aaec553c71779741
macournoyer <macournoyer@gmail.com>
2012-08-28 21:53:05 -0400

    Add TODO RE embedding inside Rails.

--- controller.rb
+++ controller.rb
@@ -2,6 +2,7 @@ require "active_record"
 require "filters"
 require "rendering"
 
+# TODO namespace everything to make work inside Rails?
 class AbstractController
   def process(action)
     send(action)
