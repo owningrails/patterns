62ecd3037e14ae4fc866dabc002ea920065ea6fe
macournoyer <macournoyer@gmail.com>
2013-02-12 17:08:01 -0500

    Implement around filters.

--- filters.rb
+++ filters.rb
@@ -5,16 +5,50 @@ module Filters
 
   module ClassMethods
     def before_filter(method)
-      before_filters << method      
+      around_filter do |controller, action|
+        controller.send method
+        action.call
+      end
     end
 
-    def before_filters
-      @before_filters ||= []
+    def after_filter(method)
+      around_filter do |controller, action|
+        action.call
+        controller.send method
+      end
+    end
+
+    def around_filters
+      @around_filters ||= []
+    end
+
+    # around_filter :method
+    # def method
+    #   yield # action.call
+    # end
+    #
+    # around_filter { |controller, action| }
+    def around_filter(method=nil, &block)
+      if block
+        around_filters << block
+      else
+        around_filters << proc { |controller, action| controller.send method, &action }
+      end
     end
   end
 
   def process(action)
-    self.class.before_filters.each { |method| send method }
-    super
+    # around_filter :one
+    # around_filter :two
+
+    # proc do # one
+    #   proc do # two
+    #     proc { super }.call
+    #   end.call
+    # end.call
+
+    self.class.around_filters.reverse.inject(proc { super }) do |parent_proc, filter|
+      proc { filter.call(self, parent_proc) }
+    end.call
   end
 end
--- tests/filter_test.rb
+++ tests/filter_test.rb
@@ -2,8 +2,11 @@ require File.dirname(__FILE__) + '/test_helper'
 require "controller"
 
 class TestController < Controller
+  around_filter :around1
   before_filter :one
   before_filter :two
+  after_filter :three
+  around_filter :around2
 
   def initialize(out)
     @out = out
@@ -17,20 +20,37 @@ class TestController < Controller
     @out << :two
   end
 
+  def three
+    @out << :three
+  end
+
+  def around1
+    @out << "1["
+    yield
+    @out << "]1"
+  end
+
+  def around2
+    @out << "2["
+    yield
+    @out << "]2"
+  end
+
   def index
     @out << :index
   end
 end
 
 class FiltersTest < Test::Unit::TestCase
-  def test_store_filters
-    assert_equal [:one, :two], TestController.before_filters
-  end
+  # def test_store_filters
+  #   assert_equal [:one, :two], TestController.before_filters
+  #   assert_equal [:three], TestController.after_filters
+  # end
 
   def test_filtering
     out = []
     TestController.new(out).process(:index)
-    assert_equal [:one, :two, :index], out
+    assert_equal ["1[", :one, :two, "2[", :index, "]2", :three, "]1"], out
   end
 end
 
