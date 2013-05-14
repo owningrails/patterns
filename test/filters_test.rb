de059cd01bb0fbf503ac4f0e84bca1a4569dc149
macournoyer <macournoyer@gmail.com>
2013-05-14 15:39:58 -0400

    Update filters tests requires.

--- test/filters_test.rb
+++ test/filters_test.rb
@@ -1,4 +1,6 @@
 require File.dirname(__FILE__) + '/test_helper'
+require "action_controller"
+require "app/controllers/application_controller"
 
 class FiltersTestController < ApplicationController
   # around_filter :around1
