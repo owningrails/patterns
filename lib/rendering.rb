a99c7808a4c1e89eaee258ea6c81c93ee202e173
macournoyer <macournoyer@gmail.com>
2013-05-15 10:12:58 -0400

    Add some starter code for Rendering.

---
+++ lib/rendering.rb
@@ -0,0 +1,11 @@
+require "erb"
+
+module Rendering
+  def template_path(action)
+    "app/views/#{controller_name}/#{action}.html.erb"
+  end
+
+  def controller_name
+    self.class.name[/^(\w+)Controller/, 1].downcase # HomeController => "home"
+  end
+end
---
+++ test/rendering_test.rb
@@ -0,0 +1,17 @@
+require File.dirname(__FILE__) + '/test_helper'
+require "action_controller"
+require "app/controllers/home_controller"
+
+class RenderingTest < Test::Unit::TestCase
+  def setup
+    @controller = HomeController.new
+  end
+
+  def test_template_path
+    assert_equal "app/views/home/index.erb", @controller.template_path("index")
+  end
+
+  def test_controller_name
+    assert_equal "home", @controller.controller_name
+  end
+end
