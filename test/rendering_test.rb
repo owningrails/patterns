2ffc2ce7274c592aa00bf07b7d827579a4a48c50
macournoyer <macournoyer@gmail.com>
2013-05-15 10:14:52 -0400

    Fix Rendering tests.

--- test/rendering_test.rb
+++ test/rendering_test.rb
@@ -1,5 +1,6 @@
 require File.dirname(__FILE__) + '/test_helper'
 require "action_controller"
+require "app/controllers/application_controller"
 require "app/controllers/home_controller"
 
 class RenderingTest < Test::Unit::TestCase
@@ -8,7 +9,7 @@ class RenderingTest < Test::Unit::TestCase
   end
 
   def test_template_path
-    assert_equal "app/views/home/index.erb", @controller.template_path("index")
+    assert_equal "app/views/home/index.html.erb", @controller.template_path("index")
   end
 
   def test_controller_name
