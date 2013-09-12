425e3a73e85fb5b3088d5127a4d86a7d4bd7e077
macournoyer <macournoyer@gmail.com>
2013-09-12 13:44:22 -0400

    Implement ActiveRecord.

---
+++ lib/active_record.rb
@@ -0,0 +1,36 @@
+require "connection_adapter"
+
+module ActiveRecord
+  class Base
+    @@connection = SqliteAdapter.new
+
+    def initialize(attributes={})
+      @attributes = attributes
+    end
+
+    def method_missing(name, *args) # args = []
+      columns = @@connection.columns(self.class.table_name)
+
+      if columns.include?(name)
+        @attributes[name]
+      else
+        super
+      end
+    end
+
+    def self.find(id)
+      find_by_sql("SELECT * FROM #{table_name} WHERE id = #{id.to_i} LIMIT 1").first
+    end
+
+    def self.find_by_sql(sql)
+      rows = @@connection.execute(sql) # [{:id=>1, :name=>"Marc", 0=>1, 1=>"Marc"}]
+      rows.map do |attributes|
+        new attributes
+      end
+    end
+
+    def self.table_name
+      name.downcase + "s" # => "users"
+    end
+  end
+end
--- test/user_test.rb
+++ test/user_test.rb
@@ -1,6 +1,21 @@
 require File.dirname(__FILE__) + '/test_helper'
+require "active_record"
 require "app/models/user"
 
 class UserTest < Test::Unit::TestCase
-  
+  def test_intialize_with_attributes
+    user = User.new(id: 1, name: "Marc")
+    assert_equal 1, user.id
+    assert_equal "Marc", user.name
+  end
+
+  def test_find
+    user = User.find(1)
+    assert_kind_of User, user
+    assert_equal 1, user.id
+  end
+
+  def test_table_name
+    assert_equal "users", User.table_name
+  end
 end
