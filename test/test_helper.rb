12e64bcafa15fa4571b3d2a6673c64532b5f2d12
macournoyer <macournoyer@gmail.com>
2014-12-23 13:37:42 -0500

    Fix minitest issue.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,10 @@
 Dir.chdir File.expand_path("../../", __FILE__)
 $LOAD_PATH.unshift ".", "lib", "test"
 
-require "test/unit"
+# Load ActiveSupport testing stuff
+require 'active_support/testing/autorun'
+require 'active_support/test_case'
+
+I18n.enforce_available_locales = true
 
 require "autoloading"
