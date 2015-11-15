1ad7c6c7aa6a34975d835bcf5706498a6f9a60ff
macournoyer <macournoyer@gmail.com>
2015-11-15 14:33:35 -0500

    Remove config/routes.rb

--- config/routes.rb
+++
@@ -1,7 +0,0 @@
-Routes = Router.new do
-  match '/' => 'home#index'
-  match '/home/index' => 'home#index'
-  match '/echo' => 'echo#index'
-  match '/users' => 'users#index'
-  match '/users/show' => 'users#show'
-end
--- lib/boot.rb
+++ lib/boot.rb
@@ -6,9 +6,6 @@ require 'bundler/setup'
 # Add our framework code (lib/) to the load path.
 $LOAD_PATH.unshift "lib"
 
-# Add the current directory to load path too so we can `require 'config/routes'` for example.
-$LOAD_PATH.unshift "."
-
 # Add all directories of app/ to the load path.
 Dir["app/*"].each do |path|
   $LOAD_PATH << path
