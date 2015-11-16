d66c80c691fe15916a04725dfc725d8fc534b860
macournoyer <macournoyer@gmail.com>
2015-11-16 13:54:57 -0500

    Implement User.all

--- lib/active_record.rb
+++ lib/active_record.rb
@@ -20,6 +20,10 @@ module ActiveRecord
       find_by_sql("SELECT * FROM #{table_name} WHERE id = #{id.to_i} LIMIT 1").first
     end
 
+    def self.all
+      find_by_sql("SELECT * FROM #{table_name}")
+    end
+
     def self.find_by_sql(sql)
       @@connection.execute(sql).map do |attributes|
         new(attributes)
